a0b78575d1cfd89ec3f0ee6ebe88ea5914094eb6
binarybabu420 <sushant09450@gmail.com>
2023-04-29 22:05:44 +0530

    todayslast

--- dom.js
+++ dom.js
@@ -35,13 +35,61 @@
 // {
 //     li[i].style.fontWeight='bold';
 // }
-var titles=document.querySelectorAll('.title');
-console.log(titles);
-titles[0]=textContent='Hello';
-var odd =document.querySelectorAll('li:nth-child(odd)');
-var even =document.querySelectorAll('li:nth-child(even)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='#f4f4f4';
-    even[i].style.backgroundColor='#ccc';
-}
+// var titles=document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0]=textContent='Hello';
+// var odd =document.querySelectorAll('li:nth-child(odd)');
+// var even =document.querySelectorAll('li:nth-child(even)');
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor='#f4f4f4';
+//     even[i].style.backgroundColor='#ccc';
+// }
+//parentNode
+// var itemlist=document.querySelector('#items');
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemlist.parentNode.parentNode.parentNode);
+//parentElement
+// var itemlist=document.querySelector('#items');
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor='#f3f3f3s';
+// console.log(itemlist.parentElement.parentElement.parentElement);
+// var itemlist=document.querySelector('#items');
+// //console.log(itemlist.children[1]);
+// itemlist.children[1].style.backgroundColor='yellow';
+// console.log(itemlist.firstChild);
+// console.log(itemlist.firstElementChild);
+var newDiv=document.createElement('div');
+//add class
+newDiv.className='hello';
+//add id
+newDiv.id='hello1';
+newDiv.setAttribute('title','hello div');
+//create text node
+var newDivText=document.createTextNode('hello World');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(container);
+console.log(h1);
+container.insertBefore(newDiv,h1);
+var nDiv=document.createElement('div');
+
+console.log(newDiv);
+console.log(nDiv);
+
+nDiv.className='hello2';
+nDiv.id='hello2';
+ nDiv.setAttribute('title','Hello world');
+ var ndivText=document.createTextNode('hello world');
+ nDiv.appendChild(ndivText);
+ var item=document.querySelectorAll('body .container');
+ console.log(item[1].children[0]);
+ var container2=document.querySelectorAll('body .title');
+ console.log(container2[1]);
+
+// 
+// newDiv.style.fontSize='30px';
+
+ item[1].children[0].insertBefore(nDiv,container2[1]);
