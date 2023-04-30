9fdd854ff31c41e853f4066bfbbf770ab88b494a
binarybabu420 <sushant09450@gmail.com>
2023-04-30 16:33:21 +0530

    newday

--- dom.js
+++ dom.js
@@ -60,36 +60,70 @@
 // itemlist.children[1].style.backgroundColor='yellow';
 // console.log(itemlist.firstChild);
 // console.log(itemlist.firstElementChild);
-var newDiv=document.createElement('div');
-//add class
-newDiv.className='hello';
-//add id
-newDiv.id='hello1';
-newDiv.setAttribute('title','hello div');
-//create text node
-var newDivText=document.createTextNode('hello World');
-newDiv.appendChild(newDivText);
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-console.log(container);
-console.log(h1);
-container.insertBefore(newDiv,h1);
-var nDiv=document.createElement('div');
+// var newDiv=document.createElement('div');
+// //add class
+// newDiv.className='hello';
+// //add id
+// newDiv.id='hello1';
+// newDiv.setAttribute('title','hello div');
+// //create text node
+// var newDivText=document.createTextNode('hello World');
+// newDiv.appendChild(newDivText);
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+// console.log(container);
+// console.log(h1);
+// container.insertBefore(newDiv,h1);
+// var nDiv=document.createElement('div');
 
-console.log(newDiv);
-console.log(nDiv);
+// console.log(newDiv);
+// console.log(nDiv);
 
-nDiv.className='hello2';
-nDiv.id='hello2';
- nDiv.setAttribute('title','Hello world');
- var ndivText=document.createTextNode('hello world');
- nDiv.appendChild(ndivText);
- var item=document.querySelectorAll('body .container');
- console.log(item[1].children[0]);
- var container2=document.querySelectorAll('body .title');
- console.log(container2[1]);
+// nDiv.className='hello2';
+// nDiv.id='hello2';
+//  nDiv.setAttribute('title','Hello world');
+//  var ndivText=document.createTextNode('hello world');
+//  nDiv.appendChild(ndivText);
+//  var item=document.querySelectorAll('body .container');
+//  console.log(item[1].children[0]);
+//  var container2=document.querySelectorAll('body .title');
+//  console.log(container2[1]);
 
-// 
-// newDiv.style.fontSize='30px';
+// // 
+// // newDiv.style.fontSize='30px';
 
- item[1].children[0].insertBefore(nDiv,container2[1]);
+//  item[1].children[0].insertBefore(nDiv,container2[1]);
+var form =document.getElementById('addForm');
+var itemlist=document.getElementById('items');
+form.addEventListener('submit',addItem);
+//delete event
+itemlist.addEventListener('click',removeItem);
+function addItem(e)
+{
+
+    e.preventDefault();
+    var newItem=document.getElementById('item').value;
+    //create new li element
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    console.log(li);
+    //add textnode with input value
+    li.appendChild(document.createTextNode(newItem));
+    var deleteBtn=document.createElement('button');
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'))
+ li.appendChild(deleteBtn);
+    itemlist.appendChild(li);
+}
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete')){
+    console.log(1);
+    if(confirm('Are you Sure?'))
+    {
+        var li=e.target.parentElement;
+        itemlist.removeChild(li);
+    }
+}
+   // console.log();
+}
