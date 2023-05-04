d8d5594537e5a224b1848e491b7ea96b34f19c59
binarybabu420 <sushant09450@gmail.com>
2023-05-04 12:19:50 +0530

    tomoreUser

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 
 var form=document.getElementById('my-form');
 form.addEventListener('submit',saveLocal);
-let myObj={
-    name:'Domenic',
-    age:56
-} 
+ 
 function saveLocal(e)
 {
     e.preventDefault();
-    // var name=document.getElementById('name').value;
+    // var name=;
     // var email=document.getElementById('email').value;
-    localStorage.setItem('myObj',JSON.stringify(myObj));
-    console.log(localStorage.getItem(myObj).name);
+    let myObj={
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        phone:document.getElementById('phone').value
+    };
+    var ul=document.getElementById('users');
+    var li=document.createElement('li');
+    li.className="item";
+    var myJSON=JSON.stringify(myObj);
+    localStorage.setItem(myObj.email,myJSON);
+    var text=localStorage.getItem(myObj.email);
+    var obj=JSON.parse(text);
+    //console.log(obj.name);
+    var st=
+    li.appendChild(document.createTextNode(obj.name+" "+obj.email+" "+obj.phone));
+    ul.appendChild(li);
+    
+    
+    console.log(document.getElementsByTagName('li'));
 }
+
