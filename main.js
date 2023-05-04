0319d3556aa36de8d80379866b234206f83597cf
binarybabu420 <sushant09450@gmail.com>
2023-05-04 10:36:01 +0530

    main1

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 
 var form=document.getElementById('my-form');
 form.addEventListener('submit',saveLocal);
+let myObj={
+    name:'Domenic',
+    age:56
+} 
 function saveLocal(e)
 {
     e.preventDefault();
-    var name=document.getElementById('name').value;
-    var email=document.getElementById('email').value;
-    localStorage.setItem(name,email);
-    console.log(localStorage.getItem(name));
+    // var name=document.getElementById('name').value;
+    // var email=document.getElementById('email').value;
+    localStorage.setItem('myObj',JSON.stringify(myObj));
+    console.log(localStorage.getItem(myObj).name);
 }
