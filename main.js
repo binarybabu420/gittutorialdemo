b114cfcda2a6576b76c22fd24aa8ca72a9b1ee2e
binarybabu420 <sushant09450@gmail.com>
2023-05-04 17:02:10 +0530

    Edit Button

--- main.js
+++ main.js
@@ -21,17 +21,40 @@ function saveLocal(e)
     var obj=JSON.parse(text);
     //console.log(obj.name);
     var btn=document.createElement('button');
+    var editBtn=document.createElement('button');
+    editBtn.appendChild(document.createTextNode('Edit'));
     li.appendChild(document.createTextNode(obj.name+" "+obj.email+" "+obj.phone));
     btn.appendChild(document.createTextNode('Delete'));
+    
     li.appendChild(btn);
+    li.appendChild(editBtn);
     ul.appendChild(li);
     btn.addEventListener('click',removeIt);
+    editBtn.addEventListener('click',editIt);
+    function editIt(e)
+    {
+        e.preventDefault();
+        var nameEdit=document.getElementById('name');
+        var emailEdit=document.getElementById('email');
+        var phoneEdit=document.getElementById('phone');
+        nameEdit.value=obj.name;
+        emailEdit.value=obj.email;
+        phoneEdit.value=obj.phone;
+        var par=editBtn.parentElement;
+        par.childNodes[0].remove();
+        par.childNodes[0].remove();
+        par.childNodes[0].remove();
+        localStorage.removeItem(obj.email);
+        
+
+    }
     function removeIt(e)
     {
     e.preventDefault();
      var par=btn.parentElement;
      localStorage.removeItem(obj.email);
 
+     par.childNodes[0].remove();
      par.childNodes[0].remove();
      par.childNodes[0].remove();
      
