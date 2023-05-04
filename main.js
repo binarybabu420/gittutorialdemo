1ae466cbbe5863ac54a28227cb8239f4dd02df31
binarybabu420 <sushant09450@gmail.com>
2023-05-04 16:39:02 +0530

    Delete data from Local Storage

--- main.js
+++ main.js
@@ -20,9 +20,25 @@ function saveLocal(e)
     var text=localStorage.getItem(myObj.email);
     var obj=JSON.parse(text);
     //console.log(obj.name);
-    var st=
+    var btn=document.createElement('button');
     li.appendChild(document.createTextNode(obj.name+" "+obj.email+" "+obj.phone));
+    btn.appendChild(document.createTextNode('Delete'));
+    li.appendChild(btn);
     ul.appendChild(li);
+    btn.addEventListener('click',removeIt);
+    function removeIt(e)
+    {
+    e.preventDefault();
+     var par=btn.parentElement;
+     localStorage.removeItem(obj.email);
+
+     par.childNodes[0].remove();
+     par.childNodes[0].remove();
+     
+     
+    }
+    
+
     
     
     console.log(document.getElementsByTagName('li'));
